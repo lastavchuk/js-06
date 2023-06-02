2862db9fe24b7283f674c4dd11a4491c4e04fdc8
lastavchuk <it.lastavchuk@gmail.com>
2023-06-02 20:20:02 +0300

    resolved

--- js/task-04.js
+++ js/task-04.js
@@ -3,15 +3,5 @@ const btnInc = document.querySelector('[data-action="increment"]');
 const value = document.querySelector("#value");
 
 let counterValue = 0;
-btnDec.addEventListener("click", onBtnDec);
-btnInc.addEventListener("click", onBtnInc);
-
-function onBtnDec() {
-    counterValue--;
-    value.innerHTML = counterValue;
-}
-
-function onBtnInc() {
-    counterValue++;
-    value.innerHTML = counterValue;
-}
+btnDec.addEventListener("click", () => (value.innerHTML = --counterValue));
+btnInc.addEventListener("click", () => (value.innerHTML = ++counterValue));
